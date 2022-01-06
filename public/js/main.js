f00f4fa5fc9a18f7c96343ee43d6f69689254905
davidtupang <davidprdmnsimatupang@gmail.com>
2022-01-06 19:02:27 +0700

    Kommit Ketiga

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,21 @@
-const socket = io();
+const chatForm  = document.getElementById('chat-form');
 
+
+const socket = io();
 
 socket.on('message', message => {
 console.log(message); 
+});
+
+//Message Submit 
+chatForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+
+
+    //Get Message Text
+    const msg  = e.target.elements.msg.value;
+    
+    //Emit Message to Serve
+    socket.emit('chatMessage',msg);
+
 });
