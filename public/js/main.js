caa35caf4c9dcb924b26d6ca99e0e3e872712df7
davidtupang <davidprdmnsimatupang@gmail.com>
2022-01-07 00:39:01 +0700

    FINISH versi 1

--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,74 @@
 const chatForm  = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
 
+// get Username And Room From URL
+const {username,room}= Qs.parse(location.search,{
+    ignoreQueryPrefix: true
+});
 
+console.log(username,room); 
 const socket = io();
 
+
+// Join Chat Room
+socket.emit('joinRoom',{username, room});
+
+// Get Room and Users
+socket.on('roomUsers',({room,users})=>{
+    outputRoomName(room);
+    outputUsers(users);
+});
+
+// Message From  Server
 socket.on('message', message => {
-console.log(message); 
+    console.log(message); 
+    outputMessage(message);
+
+    //Scroll Down
+    chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
 //Message Submit 
-chatForm.addEventListener('submit',(e)=>{
+chatForm.addEventListener('submit',e =>{
     e.preventDefault();
 
 
     //Get Message Text
     const msg  = e.target.elements.msg.value;
     
-    //Emit Message to Serve
+    //console.log(msg);
+
+    //Emit Message to Server
     socket.emit('chatMessage',msg);
 
-    //Listen For chatMessage
-    socket.on('chatMessage',msg=>{
-        console.log(msg);
-    })
+    //Clear Input
+    e.target.elements.msg.value ='';
+    e.target.elements.msg.focus();
+
 
 });
+
+//Ouput Message To DOM
+function outputMessage(message){
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML =`<p class="meta">${message.username}<span>${message.time}</span></p>
+    <p class="text">
+        ${message.text}
+    </p>`;
+    document.querySelector('.chat-messages').appendChild(div)
+}
+
+// Add room name to DOM
+function outputRoomName(room){
+    roomName.innerText =room;
+} 
+
+// Add users to DOM
+function outputUsers(users){
+    userList.innerHTML = `
+    ${users.map(user =>`<li>${user.username}</li>`).join('')}
+    `;
+}
--- server.js
+++ server.js
@@ -2,29 +2,65 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+const {userJoin, getCurrentUser, userLeave, getRoomUsers} = require('./utils/users');
+
 
 const app =express();
 const server = http.createServer(app);
 const io = socketio(server);    
 
+
 // Set Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
+const botName = 'ChatCord Bot';
 
 //Run When Client Connection
 io.on('connection',socket =>{
-   
-    //Run current user 
-    socket.emit('message', 'Wellcome to ChatCord!');
+
+        socket.on('joinRoom',({username,room})=>{
+            
+        const user = userJoin(socket.id,username,room);
+
+        socket.join(user.room);
+
+        //Welcome Current user 
+        socket.emit('message', formatMessage(botName,'Wellcome to ChatCord!'));
+
+        //Broadcast when a user connect
+        socket.broadcast.to(user.room).emit('message',  formatMessage(botName,`${user.username} has joined the Chat`));
+        
+        //Send Users and room Info
+        io.to(user.room).emit('roomUsers',{
+            room: user.room,
+            users: getRoomUsers(user.room)
+        });
+
+    });
+
     
-    //Broadcast when a user connect
-    socket.broadcast.emit('message', 'A User has joined the Chat');
+   
+    //Listen For Chat Message
+    socket.on('chatMessage',msg=>{
+        const user = getCurrentUser(socket.id);
+        //console.log(msg);
+        io.to(user.room).emit('message', formatMessage(user.username,msg));
+    });
 
     //Run When Client Disconnect
     socket.on('disconnect',()=>{
-    io.emit('message','A User has left the Chat');
-    });
-});
+    const user = userLeave(socket.id);
 
+    if(user){   
+        io.to(user.room).emit('message', formatMessage(botName,`${user.username} has left the Chat`));
+        io.to(user.room).emit('roomUsers',{
+            room: user.room,
+            users: getRoomUsers(user.room)
+        });
+    }
+    });
+}); 
+ 
 
 const PORT = 3000 || process.env.PORT;
 
